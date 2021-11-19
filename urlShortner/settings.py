faa234be4f51db4932e761384ea80e3524ac787d
Tej Pratap <tejpartap957@gmail.com>
2021-11-19 18:42:25 +0530

    master

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-entwu=w#s12miom#r8hzl2g*6_-x6=iyo$33@qwv9e0q@dmyff'
+SECRET_KEY = config('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://weely1.herokuapp.com/','http://127.0.0.1:8000/']
 
