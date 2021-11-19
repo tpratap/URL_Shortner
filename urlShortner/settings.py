566c6537f0862d33145351bab7e6767a755bc7d0
Tej Pratap <tejpartap957@gmail.com>
2021-11-19 17:01:17 +0530

    master

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-entwu=w#s12miom#r8hzl2g*6_-x6=iyo$33@qwv9e0q@dmyff'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://create-short-link.herokuapp.com/','http://127.0.0.1:8000/']
 
